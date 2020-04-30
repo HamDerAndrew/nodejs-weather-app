e60567049b6d0d3173fb48e6fe5ce369bd646d56
André Larsen <andrewsondergaard@gmail.com>
2020-04-30 17:39:26 +0200

    Feature: location input provides weatherinfo

---
+++ utils/forecast.js
@@ -0,0 +1,55 @@
+const axios = require('axios');
+const forecastKey = require('./auth.js')
+
+// Note: A callback function is not required to be named 'callback' :)
+const forecast = (latitude, longitude, callback) => {
+    const apiKey = forecastKey.returnForecast()
+    const baseURL = 'http://api.weatherstack.com/'
+    const url = `${baseURL}/current?access_key=${apiKey}&query=${latitude},${longitude}`
+    
+    axios.get(url)
+    .then(( response ) => {
+        if (response.data.error) {
+            const error = response.data.error;
+            //the callback here is adding data to the arguments in app.js 
+            callback({
+                errorCode: error.code,
+                errorType: error.type,
+                errorInfo: error.info
+            }, undefined)
+        } else {
+            // callback(undefined, {
+            //     latitude: response.data.location.lat,
+            //     longitude: response.data.location.lon
+            // })
+            //Alternative:
+            callback(undefined, `${response.data.current.weather_descriptions[0]}. It is currently ${response.data.current.temperature} degrees out. It feels like ${response.data.current.feelslike} degrees in ${response.data.location.country}.`)
+        }
+    })
+    .catch(( error ) => {
+        callback('Unable to connect to the service', undefined)
+        console.log(error)
+    })
+}
+
+module.exports = forecast;
+
+
+/*
+The callback function is passed two arguments in app.js: error, data. (it's the same function in app.js as in the bottom of this comment)
+These are the arguments we pass in values for in the forecast function.
+
+Example 1: 
+When we add 'callback' on line 14 the 'undefined' is the 'data' argument in the forecast function below.
+The data is 'undefined' because this is the code that will run if an error occours. Therefore we pass an object with errorcodes. 
+The error argument can be any datatype, but in this case it's an object
+
+Example 2:
+When we add 'callback' on line 20 the 'undefined' is the 'error' argument in the forecast function below
+The error is 'undefined' because this is the code that will run if there are no errors and we get data
+
+forecast(-75.7088, 44.1545, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+  })
+*/
--- utils/geocode.js
+++ utils/geocode.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
+const geocodeKey = require('./auth')
 
 const geocode = (address, callback) => {
-    const token = 'pk.eyJ1IjoiYWxhcnNlbiIsImEiOiJjazlsOWI4NWswMmZoM21tdGZvaWk5a3FvIn0.aBWh-pC7cINpuAhHp3_lYw'
+    const token = geocodeKey.returnGeocode()
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${token}&limit=1`
 
     axios.get(url)
@@ -11,7 +12,7 @@ const geocode = (address, callback) => {
             // The code would work even without defining 'undefined', but in this case it's there to showcase it.
             callback("Invalid search term. Can't find location. Try another", undefined)
         } else {
-            // now the address is undefined by default and we preceed to use the callback function to add data into an object.
+            // now the address is undefined by default and we proceed to use the callback function to add data into an object.
             callback(undefined, {
                 long: response.data.features[0].center[0],
                 lat: response.data.features[0].center[1],
@@ -25,4 +26,5 @@ const geocode = (address, callback) => {
     })
 }
 
-module.exports = geocode
+
+module.exports = geocode;
